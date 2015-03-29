39432ad5d329416057747e3379bd7fe72e2b230c
Pushkar Godbole <pushkar.godbole@gatech.edu>
2015-03-29 01:14:50 -0400

    Added get_characters.py and awoiaf_loc.txt

---
+++ DataGen/get_characters.py
@@ -0,0 +1,28 @@
+
+
+def get_characters(file):
+    f = open(file, 'r')
+    if(f):
+        characters = []
+        endline='Retrieved from'
+        for line in f:
+            if len(line) > 3:
+                if line[:3] == '  *':
+                    elems = line.split()
+                    for elem in elems:
+                        if elem.find('</index.php') >= 0:
+                            elem = elem.strip('</>,')
+                            character = elem.split('/')[1]
+                            characters.append(character)
+                            break
+            if line.find(endline) >= 0:
+                break
+                
+        f.close()
+        return characters
+        
+if __name__ == "__main__":
+    file = "../Data/awoiaf_loc.txt"
+    characters = get_characters(file)
+    for i in range(len(characters)):
+        print str(i+1)+'.', characters[i]
