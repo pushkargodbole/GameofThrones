1fe532293989096f48094f24b342ec85715ee2b0
PARITOSH GOTE <paritosh.gote@gatech.edu>
2015-04-21 20:16:05 -0400

    Adding corrected analyseSubjective

--- DataGen/analyseSubjective.py
+++ DataGen/analyseSubjective.py
@@ -4,28 +4,28 @@ from pprint import pprint
 from get_personalities import get_personalities
 from nltk import PorterStemmer
 
-with open( subjectiveFeatures.json ) as data_file:    
+with open('subjectiveFeatures.json') as data_file:    
     data = json.load(data_file)
 
 
-physical_features = [ hair , eyes , face , beard , skin , flesh ,  accent ,  nose , legs ,  shoulders , stomach , jaw , ears , nipples , laugh , smile , lips , cheekbones , height , chin , mustache , scales , build , voice , eyebrows , appetite , chest , grasp , temper , figure , hairs , tongue , mouth , hands , bosom , breasts , belly , whiskers , cheeks , size , body ]
+physical_features = ['hair','eyes','face','beard','skin','flesh', 'accent', 'nose','legs', 'shoulders','stomach','jaw','ears','nipples','laugh','smile','lips','cheekbones','height','chin','mustache','scales','build','voice','eyebrows','appetite','chest','grasp','temper','figure','hairs','tongue','mouth','hands','bosom','breasts','belly','whiskers','cheeks','size','body']
 personality_stems = []
 
 
-personalities = get_personalities([ ../Data/traits1.txt ,  ../Data/traits2.txt ])
+personalities = get_personalities(['../Data/traits1.txt', '../Data/traits2.txt'])
 used_personalities = []
 
-objective_characteristics = [ Allegiance , Culture , Birthyear , Deathyear , Age, Gender , DeadAlive ]
+objective_characteristics = ['Allegiance','Culture','Birthyear','Deathyear', 'Age' ,'Gender','DeadAlive']
 
 
 	#for row in reader:
-	#	print(row[ Character name ], row[ Deathyear ])
-	#print reader[ Character name ]
+	#	print(row['Character name'], row['Deathyear'])
+	#print reader['Character name']
 
 
-with open( characteristics.csv ,  ab ) as csvfile:
-	spamwriter = csv.writer(csvfile, delimiter= , , quotechar= | , quoting=csv.QUOTE_MINIMAL)
-	spamwriter.writerow([ Name ]+personalities+objective_characteristics)
+with open('characteristics.csv', 'ab') as csvfile:
+	spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	spamwriter.writerow(['Name']+personalities+objective_characteristics)
 
    	for character in data["Characters"]:
 		#print character["Name"],", "
@@ -38,11 +38,11 @@ with open( characteristics.csv ,  ab ) as csvfile:
 				if character["Characteristics"][personality] != None:
 					char_personality[personalities.index(personality)] = int(character["Characteristics"][personality])
 
-		with open( character_features.csv ) as csvfile2:
+		with open('character_features.csv') as csvfile2:
 			reader = csv.DictReader(csvfile2)
 			for row in reader:
-				#print row[ Character name ],character
-				if row[ Character name ] == character[ Name ]:
+				#print row['Character name'],character
+				if row['Character name'] == character['Name']:
 					for obj_char in objective_characteristics:
 						if row[obj_char] != "NULL":
 							char_objective[objective_characteristics.index(obj_char)] = row[obj_char]
